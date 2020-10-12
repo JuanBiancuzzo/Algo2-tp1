52d0c581afb22f1410df6ed5f01dd8befa19bda6
juanh <juanhbiancuzzo>
2020-10-12 11:25:13 -0300

    estaba verificando que funcionara la funcion crear_arrecife, por ahora se ve que funciona bien pero tengo que probarla mas

--- evento_pesca.c
+++ evento_pesca.c
@@ -5,7 +5,7 @@
 #define FORMATO "%[^;];%i;%i;%[^;]"
 
 int leer_pokemon(FILE* archivo, pokemon_t* pokemon) {
-	return fscanf(archivo, FORMATO, &(pokemon->especie), &(pokemon->velocidad), &(pokemon->peso), &(pokemon->color));
+	return fscanf(archivo, FORMATO, pokemon->especie, &(pokemon->velocidad), &(pokemon->peso), pokemon->color);
 } 
 
 arrecife_t* crear_arrecife(const char* ruta_archivo) {
@@ -13,6 +13,9 @@ arrecife_t* crear_arrecife(const char* ruta_archivo) {
 	arrecife_t* arrecife = malloc(sizeof(arrecife_t));
 	pokemon_t* p_pokemon = NULL;
 	pokemon_t pokemon_aux;
+
+	if (arrecife == NULL)
+		return arrecife;
 
 	FILE* archivo = fopen(ruta_archivo, "r");
 
@@ -23,9 +26,9 @@ arrecife_t* crear_arrecife(const char* ruta_archivo) {
 
 	while (leido == 4) {
 
-		(*arrecife).pokemon = realloc(p_pokemon, (*arrecife).cantidad_pokemon + 1);
-		(*((arrecife).pokemon)+(*arrecife).cantidad_pokemon) = pokemon_aux;
-		((*arrecife).cantidad_pokemon)++;
+		arrecife->pokemon = realloc(p_pokemon, (size_t) (arrecife->cantidad_pokemon + 1) * sizeof(pokemon_t));
+		(*(arrecife->pokemon + arrecife->cantidad_pokemon)) = pokemon_aux;
+		(arrecife->cantidad_pokemon)++;
 
 		leido = leer_pokemon(archivo, &pokemon_aux);
 	}
@@ -33,13 +36,20 @@ arrecife_t* crear_arrecife(const char* ruta_archivo) {
 	return arrecife;
 }
 
-acuario_t* crear_acuario() {}
+acuario_t* crear_acuario() {
+	acuario_t* acuario = malloc(sizeof(acuario_t));
+	return acuario;
+}
 
-int trasladar_pokemon(arrecife_t* arrecife, acuario_t* acuario, bool (*seleccionar_pokemon) (pokemon_t*), int cant_seleccion) {}
+int trasladar_pokemon(arrecife_t* arrecife, acuario_t* acuario, bool (*seleccionar_pokemon) (pokemon_t*), int cant_seleccion) {
+	return 0;
+}
 
 void censar_arrecife(arrecife_t* arrecife, void (*mostrar_pokemon)(pokemon_t*)) {}
 
-int guardar_datos_acuario(acuario_t* acuario, const char* nombre_archivo) {}
+int guardar_datos_acuario(acuario_t* acuario, const char* nombre_archivo) {
+	return 0;
+}
 
 void liberar_acuario(acuario_t* acuario) {}
 
